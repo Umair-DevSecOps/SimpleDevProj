5c8cff38b2206c1dde27162b5fc0c477856024bd
Umair Ali <umair9009@hotmail.com>
2026-05-04 02:08:15 +0100

    fix azure port binding

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello DevOps Engineer Apprentice!!!!!"
+    return "App is running!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 80))
+    app.run(host="0.0.0.0", port=port)
